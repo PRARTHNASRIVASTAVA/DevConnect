8aeee1f505b4b43616edbdbe83d5c67f4fb53d3a
PRARTHNASRIVASTAVA <prarthnasrivastava2@gmail.com>
2023-07-17 11:51:34 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import Home from './components/Home';
+//  import { Component } from 'react';
+import Login from './components/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Signup from './components/Signup';
+// import { Toaster } from 'react';
+import AddBlog from './components/AddBlog';
+import ViewBlog from './components/ViewBlog';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Toaster position = 'top-center' /> */}
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+
+          <Route element={<Home />} path='home' />
+          <Route element={<Login />} path='login' />
+          <Route element={<Signup />} path='signup' />
+          <Route element={<AddBlog />} path='addblog' />
+          <Route element={<ViewBlog />} path='viewblog' />
+          <Route element={<Profile />} path='profile' />
+
+
+        </Routes>
+
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container'>
+    <div className="main">
+      <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YmxvZ2dpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+    <h1 className='text-center'> Developers Blogging</h1>
+        
+    </div>
+        
+
+
+
+    </div>
+  )
+}
+
+export default Home
