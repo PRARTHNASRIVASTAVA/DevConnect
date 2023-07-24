22a7a187d3aeef7de81f40747f33ca3ce714593c
yourusername <triplem656@gmail.com>
2023-07-24 14:35:10 +0530

    ..

--- devconnect_frontend/src/App.js
+++ devconnect_frontend/src/App.js
@@ -1,37 +1,43 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-import Home from './components/Home';
+import Home from "./components/Home";
 //  import { Component } from 'react';
-import Login from './components/Login';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Signup from './components/Signup';
+import Login from "./components/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Signup from "./components/Signup";
 // import { Toaster } from 'react';
-import AddBlog from './components/AddBlog';
-import ViewBlog from './components/ViewBlog';
-import Profile from './components/Profile';
+import AddBlog from "./components/AddBlog";
+import ViewBlog from "./components/ViewBlog";
+import Profile from "./components/Profile";
+import { UserProvider } from "./UserContext";
+import UserAuth from "./UserAuth";
 
 function App() {
   return (
     <div>
       {/* <Toaster position = 'top-center' /> */}
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-
-          <Route element={<Home />} path='home' />
-          <Route element={<Login />} path='login' />
-          <Route element={<Signup />} path='signup' />
-          <Route element={<AddBlog />} path='addblog' />
-          <Route element={<ViewBlog />} path='viewblog' />
-          <Route element={<Profile />} path='profile' />
-
-
-        </Routes>
-
+        <UserProvider>
+          <Navbar />
+          <Routes>
+            <Route element={<Home />} path="home" />
+            <Route element={<Login />} path="login" />
+            <Route element={<Signup />} path="signup" />
+            <Route
+              element={
+                <UserAuth>
+                  <AddBlog />
+                </UserAuth>
+              }
+              path="addblog"
+            />
+            <Route element={<ViewBlog />} path="viewblog" />
+            <Route element={<Profile />} path="profile" />
+          </Routes>
+        </UserProvider>
       </BrowserRouter>
-
     </div>
   );
 }
